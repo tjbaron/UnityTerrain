d0a5b2d92866e0c389461ca920da070fd271cafb
Thomas Baron <thomasjbaron@hotmail.com>
2015-12-03 00:13:41 -0700

    Animate seed to simulate tectonics. Subdivision works!

---
+++ Assets/Scripts/Planet/AnimateSeed.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateSeed : MonoBehaviour {
+	public Planet planet;
+	private float lastChange = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lastChange += Time.deltaTime;
+		if (lastChange > 0.3f) {
+			planet.displacementLayers[0].seed += 0.05f;
+			planet.updateTerrain = true;
+			lastChange = 0.0f;
+		}
+	}
+}
--- Assets/TerrainGenerator/Planet.cs
+++ Assets/TerrainGenerator/Planet.cs
@@ -24,8 +24,47 @@ public class Planet : MonoBehaviour {
 	public Material waterMaterial;
 	public DisplacementLayer[] displacementLayers;
 
+	private Vector3[][] cubeSides = new Vector3[][]{
+		new Vector3[]{
+			new Vector3(-1f,-1f, 1f),
+			new Vector3( 1f,-1f, 1f),
+			new Vector3(-1f, 1f, 1f),
+			new Vector3( 1f, 1f, 1f)
+		},
+		new Vector3[]{
+			new Vector3(-1f, 1f, 1f), 
+			new Vector3( 1f, 1f, 1f),
+			new Vector3(-1f, 1f,-1f),
+			new Vector3( 1f, 1f,-1f)
+		},
+		new Vector3[]{
+			new Vector3(-1f,-1f,-1f), 
+			new Vector3( 1f,-1f,-1f),
+			new Vector3(-1f,-1f, 1f),
+			new Vector3( 1f,-1f, 1f)
+		},
+		new Vector3[]{
+			new Vector3( 1f,-1f,-1f), 
+			new Vector3( 1f, 1f,-1f),
+			new Vector3( 1f,-1f, 1f),
+			new Vector3( 1f, 1f, 1f)
+		},
+		new Vector3[]{
+			new Vector3(-1f,-1f, 1f), 
+			new Vector3(-1f, 1f, 1f),
+			new Vector3(-1f,-1f,-1f),
+			new Vector3(-1f, 1f,-1f)
+		},
+		new Vector3[]{
+			new Vector3( 1f,-1f,-1f), 
+			new Vector3(-1f,-1f,-1f),
+			new Vector3( 1f, 1f,-1f),
+			new Vector3(-1f, 1f,-1f)
+		}
+	};
+
 	// Use this for initialization
-	void Update () {
+	void Update() {
 		if (updateTerrain) {
 			var children = new List<GameObject>();
 			foreach (Transform child in transform) children.Add(child.gameObject);
@@ -39,51 +78,46 @@ public class Planet : MonoBehaviour {
 
 	void MakeSphere(float rad, DisplacementLayer[] displace=null) {
 		for (var i=0; i<6; i++) {
-			var go = new GameObject();
-			var mf = go.AddComponent<MeshFilter>();
-			var mr = go.AddComponent<MeshRenderer>();
-			var mc = go.AddComponent<MeshCollider>();
-			if (i == 0) {
+			MakeSegment(1, cubeSides[i], rad, displace);
+		}
+	}
+
+	void MakeSegment(int subdiv, Vector3[] v, float rad, DisplacementLayer[] displace=null) {
+		// Find the 5 points that split this quad into 4 more.
+		var top = (v[1]+v[0])/2f;
+		var right = (v[3]+v[1])/2f;
+		var bottom = (v[2]+v[3])/2f;
+		var left = (v[0]+v[2])/2f;
+		var mid = (v[3]+v[0])/2f;
+		// Define the 4 new quads using existing and new points.
+		var segs = new Vector3[][]{
+			new Vector3[]{v[0], top, left, mid},
+			new Vector3[]{top, v[1], mid, right},
+			new Vector3[]{left, mid, v[2], bottom},
+			new Vector3[]{mid, right, bottom, v[3]}
+		};
+
+		for (var i=0; i<segs.Length; i++) {
+			if (subdiv == 1) {
+				var seg = segs[i];
+				var go = new GameObject();
+				var mf = go.AddComponent<MeshFilter>();
+				var mr = go.AddComponent<MeshRenderer>();
+				var mc = go.AddComponent<MeshCollider>();
+
 				mf.sharedMesh = Segment.Generate(segmentResolution, rad, 
-				new Vector3(-0.5f,-0.5f,0.5f), 
-				new Vector3(0.5f,-0.5f,0.5f),
-				new Vector3(-0.5f,0.5f,0.5f),
-				new Vector3(0.5f,0.5f,0.5f), displace);
-			} else if (i == 1) {
-				mf.sharedMesh = Segment.Generate(segmentResolution, rad, 
-				new Vector3(-0.5f,0.5f,0.5f), 
-				new Vector3(0.5f,0.5f,0.5f),
-				new Vector3(-0.5f,0.5f,-0.5f),
-				new Vector3(0.5f,0.5f,-0.5f), displace);
-			} else if (i == 2) {
-				mf.sharedMesh = Segment.Generate(segmentResolution, rad, 
-				new Vector3(-0.5f,-0.5f,-0.5f), 
-				new Vector3(0.5f,-0.5f,-0.5f),
-				new Vector3(-0.5f,-0.5f,0.5f),
-				new Vector3(0.5f,-0.5f,0.5f), displace);
-			} else if (i == 3) {
-				mf.sharedMesh = Segment.Generate(segmentResolution, rad, 
-				new Vector3(0.5f,-0.5f,-0.5f), 
-				new Vector3(0.5f,0.5f,-0.5f),
-				new Vector3(0.5f,-0.5f,0.5f),
-				new Vector3(0.5f,0.5f,0.5f), displace);
-			} else if (i == 4) {
-				mf.sharedMesh = Segment.Generate(segmentResolution, rad, 
-				new Vector3(-0.5f,-0.5f,0.5f), 
-				new Vector3(-0.5f,0.5f,0.5f),
-				new Vector3(-0.5f,-0.5f,-0.5f),
-				new Vector3(-0.5f,0.5f,-0.5f), displace);
+					seg[0], 
+					seg[1],
+					seg[2],
+					seg[3], displace);
+
+				if (displace != null) mr.sharedMaterial = material;
+				else mr.sharedMaterial = waterMaterial;
+				mc.sharedMesh = mf.sharedMesh;
+				go.GetComponent<Transform>().parent = transform;
 			} else {
-				mf.sharedMesh = Segment.Generate(segmentResolution, rad, 
-				new Vector3(0.5f,-0.5f,-0.5f), 
-				new Vector3(-0.5f,-0.5f,-0.5f),
-				new Vector3(0.5f,0.5f,-0.5f),
-				new Vector3(-0.5f,0.5f,-0.5f), displace);
+				MakeSegment(subdiv-1, segs[i], rad, displace);
 			}
-			if (displace != null) mr.sharedMaterial = material;
-			else mr.sharedMaterial = waterMaterial;
-			mc.sharedMesh = mf.sharedMesh;
-			go.GetComponent<Transform>().parent = transform;
 		}
 	}
 }
