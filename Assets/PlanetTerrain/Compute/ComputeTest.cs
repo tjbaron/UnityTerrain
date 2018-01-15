e34491fd9295c02478f33c109fb70eab13517bf5
Thomas Baron <thomas.baron@twofront.com>
2018-01-15 01:04:31 -0700

    Perlin noise generated and applied to texture using graphics acceleration.

---
+++ Assets/PlanetTerrain/Compute/ComputeTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputeTest : MonoBehaviour {
+
+	public ComputeShader shader;
+	public Material mat;
+
+	// Use this for initialization
+	void Start () {
+		int kernelHandle = shader.FindKernel("CSMain");
+
+		RenderTexture tex = new RenderTexture(512,512,24);
+		tex.enableRandomWrite = true;
+		tex.Create();
+		mat.SetTexture("_MainTex", tex);
+
+		shader.SetTexture(kernelHandle, "Result", tex);
+		shader.Dispatch(kernelHandle, 512/8, 512/8, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int kernelHandle = shader.FindKernel("CSMain");
+		shader.SetFloat("_Time", Time.time);
+		shader.Dispatch(kernelHandle, 512/8, 512/8, 1);
+	}
+}
